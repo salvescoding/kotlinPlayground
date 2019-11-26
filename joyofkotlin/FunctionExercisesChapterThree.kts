f93c60a4c72c76e6a9b3aef0250ed1a66db5d4c3
Sergio Alves <sergio.alves@otto.de>
2019-11-26 12:59:38 +0100

    Function composition and add git ignore

--- joyofkotlin/FunctionExercisesChapterThree.kts
+++ joyofkotlin/FunctionExercisesChapterThree.kts
@@ -8,6 +8,9 @@ val randomNumbers = listOf(3,25,12,43)
 val square: (Int) -> Int = { it * it }
 val triple: (Int) -> Int = { it * 3}
 
+val even: (Int) -> Boolean = { it % 2 == 0 }
+val booleanMapper: (Boolean) -> Int = { if(it) 1 else 0 }
+
 /**
  * Function composition that accepts two functions as
  * arguments(Receive an Argument Type A and returns the same Type)
@@ -18,9 +21,17 @@ fun <T, U, V> compose(f: (U) -> V, g: (T) -> U) : (T) -> V = { f(g(it)) }
 
 val composedFunction = compose(square, triple)
 
+val composedFunctionTwo = compose(booleanMapper, even)
+
 // Apply function composition to a list of numbe
 println("The result of the composed function applied to a list of Ints")
 println(randomNumbers.map(composedFunction))
 println("Composed function applied to a single element")
 println(composedFunction(23))
+
+println("Da")
+println("The result of function composed mapping list of numbers to 0(False) and 1(True) even numbers")
+println(randomNumbers.map(composedFunctionTwo))
+
+
 
