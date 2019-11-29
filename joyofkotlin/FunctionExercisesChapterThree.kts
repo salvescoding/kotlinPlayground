ef5370c4641ae23a13e09356bd359bfedda7a4fc
salves <salves2383@gmail.com>
2019-11-29 13:43:32 +0100

    Add HOFP with function as value

--- joyofkotlin/FunctionExercisesChapterThree.kts
+++ joyofkotlin/FunctionExercisesChapterThree.kts
@@ -23,15 +23,58 @@ val composedFunction = compose(square, triple)
 
 val composedFunctionTwo = compose(booleanMapper, even)
 
-// Apply function composition to a list of numbe
+// Apply function composition to a list of numbers
 println("The result of the composed function applied to a list of Ints")
 println(randomNumbers.map(composedFunction))
 println("Composed function applied to a single element")
 println(composedFunction(23))
 
-println("Da")
 println("The result of function composed mapping list of numbers to 0(False) and 1(True) even numbers")
 println(randomNumbers.map(composedFunctionTwo))
+
+
+/**
+ * Curried function with function as a value
+ */
+
+val add: (Int) -> (Int) -> Int = { a -> { b -> a + b} }
+
+println("Printing a curried function that adds two numbers 23 + 4")
+println(add(23)(4))
+
+
+/**
+ * Function as values
+ */
+
+val composeVal: ((Int) -> Int) -> ((Int) -> Int) -> (Int) -> Int =
+    { x -> { y -> { z -> x(y(z)) } } }
+
+val composedFuntionThree = composeVal(square)(triple)
+
+println("Function composition with first triple the value 3 and then square it")
+println(composedFuntionThree(3))
+
+println("Same function composition applied to a list of numbers 3,25,12,43")
+randomNumbers.map(composedFuntionThree).forEach(::println)
+
+
+/**
+ * Function composition as values instead of functions
+ * Genery HOFP polymorphic
+ */
+fun <T, U, V> higherCompose() =
+    { f: (U) -> V ->
+        { g: (T) -> U ->
+            { x: T -> f(g(x)) }
+        }
+    }
+println("Printing composed High order function for methods square and triple")
+
+val composedFunctionFour = higherCompose<Int, Int, Int>()(square)(triple)
+
+println("Printing HOFP with composed function as a value 14")
+println(composedFunctionFour(14))
 
 
 
