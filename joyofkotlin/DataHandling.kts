e2493f1238d2539fb1551adcfa44b898dda3af10
salves <salves2383@gmail.com>
2020-01-06 13:32:41 +0100

    Add concatenation with corecursion for 2 lists

--- joyofkotlin/DataHandling.kts
+++ joyofkotlin/DataHandling.kts
@@ -1,21 +1,26 @@
 package joyofkotlin
 
-import joyofkotlin.DataHandling.List.Companion.drop
-import joyofkotlin.DataHandling.List.Companion.dropWhile
-import java.lang.IllegalStateException
+//
+//tailrec fun sum(list: List<Int>) : Int =
+//    when(list) {
+//        is Nil -> 0
+//        is Cons -> list.head + sum(list.tail)
+//    }
 
 
 @Suppress("UNCHECKED_CAST")
-sealed class List<A> {
+sealed class List<out A> {
 
     abstract fun isEmpty(): Boolean
 
-    fun addToStart(a: A) : List<A> = Cons(a, this)
+    fun addToStart(a: @UnsafeVariance A) : List<A> = Cons(a, this)
 
-    fun setNewHead(a: A) :List<A> = when(this) {
+    fun setNewHead(a: @UnsafeVariance A) :List<A> = when(this) {
         is Nil -> throw IllegalStateException("Cannot set head on empty list")
         is Cons -> Cons(a, this.tail)
     }
+
+    fun concat(list: List<@UnsafeVariance A>): List<A> = concat(this, list)
 
     fun dropWhile(predicate: (A) -> Boolean): List<A> = dropWhile(this, predicate)
 
@@ -25,12 +30,12 @@ sealed class List<A> {
 
     fun reverse(): List<A> = reverse(invoke(), this)
 
-    private object Nil : List<Nothing>() {
+    internal object Nil : List<Nothing>() {
         override fun isEmpty() = true
         override fun toString(): String = "[NIL]"
     }
 
-    private class Cons<A>(internal val head: A, internal val tail: List<A>) : List<A>() {
+    internal class Cons<A>(internal val head: A, internal val tail: List<A>) : List<A>() {
 
         override fun isEmpty() = false
 
@@ -69,6 +74,15 @@ sealed class List<A> {
                 is Cons -> reverse(acc.addToStart(list.head), list.tail)
             }
 
+        private fun <A> concat(list: List<A>, other: List<A>) : List<A> {
+            tailrec fun concat_(listToConc: List<A>, concatList: List<A>) : List<A> =
+            when (listToConc) {
+                is Nil -> concatList
+                is Cons -> concat_(listToConc.tail, concatList.addToStart(listToConc.head))
+            }
+            return concat_(list.reverse(), other)
+        }
+
 
 
 
@@ -77,6 +91,9 @@ sealed class List<A> {
 
 val funkyList = List(1,3,4,5)
 println("Original List: $funkyList")
+
+println("Concatenating 2 lists")
+println(funkyList.concat(List(6,7,8,9)))
 
 println("Dropping first x elements from the list with creating new elements: ")
 println(funkyList.drop(2))
